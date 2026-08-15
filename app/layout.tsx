106e3dbc793db30250692240f4e1f054e8934520
MADET DIGITAL <contato@madetdigital.com.br>
2026-08-15 00:47:32 -0300

    Add Google Analytics 4

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 
 const inter = Inter({
@@ -21,6 +22,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <body className={inter.className}>{children}</body>
+      <GoogleAnalytics gaId="G-RWCZBQH3QJ" />
     </html>
   );
 }
